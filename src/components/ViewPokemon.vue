c0735bd9d406cb49845e5c72a3af08f55e823116
Carter Walsh <cartwalsh@gmail.com>
2023-11-24 12:59:04 -0600

    started on modal

--- src/components/ViewPokemon.vue
+++ src/components/ViewPokemon.vue
@@ -2,27 +2,23 @@
 import { Vue, Component } from 'vue-property-decorator';
 
 @Component({})
-export default class AddNewPoke extends Vue {
-
+export default class ViewPokemon extends Vue {
+  showPokeDetails = false;
 }
 </script>
 
 <template>
-  <b-modal title="Add New Pokemon" ok-variant="danger" cancel-variant="primary"
-           @ok="addPokemon" v-model="showConfirmDelete">
+  <b-modal title="View Pokemon" ok-variant="success" cancel-variant="danger"
+           v-model="showPokeDetails">
     <!--    using slots -- https://vuejs.org/v2/guide/components-slots.html
       slot defined in b-modal -- https://bootstrap-vue.org/docs/components/modal#comp-ref-b-modal-slots
       modify the buttons that appear in the footer of the modal using pre-defined slots-->
     <template #modal-cancel>
-      <!-- add a X icon to the cancel button-->
       <b-icon-x-square-fill /> Cancel
     </template>
-
     <template #modal-ok>
-      <!-- change the OK button to say Delete instead and add a trash can icon-->
       <b-icon-cloud-arrow-up-fill/> Add Pokemon
     </template>
-    Are you sure you want to delete {{tempStudent.familyName}}, {{tempStudent.givenName}}?
   </b-modal>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,6 +14,10 @@
       sprite="https://imgur.com/CtkIAQO"
       @viewpoke="showPokeModal()"
     />
+    <ViewPokemon
+      @ok="savePokemon"
+      v-model="view"
+    />
 
   </div>
 </template>
@@ -22,10 +26,14 @@
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue';
 import IconButton from '@/components/IconButton.vue';
-import PokeCard from '@/components/PokeCard.vue'; // @ is an alias to /src
+import PokeCard from '@/components/PokeCard.vue';
+import ViewPokemon from '@/components/ViewPokemon.vue';
+import { BIcon } from 'bootstrap-vue';
+import Student from '@/models/Student'; // @ is an alias to /src
 
 @Component({
   components: {
+    ViewPokemon,
     PokeCard,
     IconButton,
     HelloWorld,
@@ -38,7 +46,33 @@ export default class HomeView extends Vue {
   }
 
   showPokeModal():void {
-    alert('pokemon');
+    // <ViewPokemon />
+  }
+
+  savePokemon() {
+    const icon:BIcon = this.$refs.iconSave; // get the icon to animate from the vue refs https://vuejs.org/v2/api/#ref
+    this.setBusy(true);// tell parent that this component is waiting for the api to respond
+    this.animate(icon, true);// animate the icon in the clicked button to give the user an indication that some thing is happening
+    this.violation = {};// empty out violation messages - to hide violation message from user and wait for new violations from the api
+
+    // use the method declared in the student mixin to call the student api
+    // if the student is new POST , if the student already exists then PUT
+    const url = this.STUDENT_API + (this.isNew ? '' : `/${this.tempStudent.id}`);
+    const method = this.isNew ? 'post' : 'put';
+
+    this.callAPI(url, method, this.tempStudent) // returns a promise object
+      .then((data) => {
+        // determine if the student was added or updated
+        this.$emit(this.tempStudent.id === data.id ? 'updated' : 'added', data);
+      })
+      .catch((err) => {
+        // get the violation messages from the api - if the web server responded
+        this.violation = err.data || {};
+      })
+      .finally(() => {
+        this.setBusy(false);// tell parent that this component is no longer waiting for the api
+        this.animate(icon, false);// stop the icon animation
+      });
   }
 }
 </script>
